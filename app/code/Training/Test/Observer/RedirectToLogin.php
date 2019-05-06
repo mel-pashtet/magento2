29958147cdb1ad2643fe7baac07d8635a43e6279
mel-pashtet <mel.pashtet@gmail.com>
2019-05-06 23:08:51 +0300

    issue 1.10

---
+++ app/code/Training/Test/Observer/RedirectToLogin.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavelmelnichuk
+ * Date: 5/6/19
+ * Time: 10:54 PM
+ */
+
+namespace Training\Test\Observer;
+
+
+use Magento\Framework\Event\ObserverInterface;
+class RedirectToLogin implements ObserverInterface
+{
+    /**
+     * @var \Magento\Framework\App\Response\RedirectInterface
+     */
+    private $redirect;
+    /**
+     * @var \Magento\Framework\App\ActionFlag
+     */
+    private $actionFlag;
+    /**
+     * @param \Magento\Framework\App\Response\RedirectInterface $redirect
+     * @param \Magento\Framework\App\ActionFlag $actionFlag
+     */
+    public function __construct(
+        \Magento\Framework\App\Response\RedirectInterface $redirect,
+        \Magento\Framework\App\ActionFlag $actionFlag
+    ) {
+        $this->redirect = $redirect;
+        $this->actionFlag = $actionFlag;
+    }
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $request = $observer->getEvent()->getData('request');
+        if ($request->getModuleName() == 'catalog'
+            && $request->getControllerName() == 'product'
+            && $request->getActionName() == 'view'
+        ) {
+            // if ($request->getFullActionName() == 'catalog_product_view') { // altenative way
+            $controller = $observer->getEvent()->getData('controller_action');
+            $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
+
+        }
+    }
+}
