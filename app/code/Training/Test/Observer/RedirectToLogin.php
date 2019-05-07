989c40e54c46d68638eb35b57a4936baec86169f
melpashtet <pavlo@frendino.com>
2019-05-07 09:25:26 +0300

    issue 1.10(small fix)

--- app/code/Training/Test/Observer/RedirectToLogin.php
+++ app/code/Training/Test/Observer/RedirectToLogin.php
@@ -12,6 +12,7 @@ namespace Training\Test\Observer;
 use Magento\Framework\Event\ObserverInterface;
 class RedirectToLogin implements ObserverInterface
 {
+    protected $customerSession;
     /**
      * @var \Magento\Framework\App\Response\RedirectInterface
      */
@@ -25,23 +26,28 @@ class RedirectToLogin implements ObserverInterface
      * @param \Magento\Framework\App\ActionFlag $actionFlag
      */
     public function __construct(
+        \Magento\Customer\Model\Session $customerSession,
         \Magento\Framework\App\Response\RedirectInterface $redirect,
         \Magento\Framework\App\ActionFlag $actionFlag
     ) {
+        $this->customerSession = $customerSession;
         $this->redirect = $redirect;
         $this->actionFlag = $actionFlag;
     }
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $request = $observer->getEvent()->getData('request');
-        if ($request->getModuleName() == 'catalog'
-            && $request->getControllerName() == 'product'
-            && $request->getActionName() == 'view'
-        ) {
-            // if ($request->getFullActionName() == 'catalog_product_view') { // altenative way
+        if (!$this->customerSession->isLoggedIn()) {
+
+//            I disable this block, because I have direct event(controller_action_predispatch_catalog_product_view)
+
+//            $request = $observer->getEvent()->getData('request');
+//            if ($request->getFullActionName() == 'catalog_product_view') { // altenative way
+//                $controller = $observer->getEvent()->getData('controller_action');
+//                $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
+//            }
+
             $controller = $observer->getEvent()->getData('controller_action');
             $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
-
         }
     }
 }
