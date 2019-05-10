fe6226284deadcc775b6b8d20ddf5c465107ca60
melpashtet <pavlo@frendino.com>
2019-05-10 17:58:18 +0300

    issue 3.2

---
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Training\Test\Controller\Block;
+
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\LayoutFactory
+     */
+    private $layoutFactory;
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\LayoutFactory $layoutFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\LayoutFactory $layoutFactory
+    ) {
+        $this->layoutFactory = $layoutFactory;
+        parent::__construct($context);
+    }
+    public function execute()
+    {
+        $layout = $this->layoutFactory->create();
+        $block = $layout->createBlock('Training\Test\Block\Test');
+        return $this->getResponse()->appendBody($block->toHtml());
+
+    }
+}
