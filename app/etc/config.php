2e519a297d33d4015433cb74d2311e855c594790
melpashtet <pavlo@frendino.com>
2019-06-07 17:36:05 +0300

    issue 5.1

--- app/etc/config.php
+++ app/etc/config.php
@@ -226,6 +226,7 @@ return [
         'Temando_Shipping' => 1,
         'Training_Training_TestOM' => 1,
         'Training_Render' => 1,
+        'Training_Feedback' => 1,
         'Training_Test' => 1,
         'Training_Training_Dependency2' => 0,
         'Training_Training_Dependency' => 1,
